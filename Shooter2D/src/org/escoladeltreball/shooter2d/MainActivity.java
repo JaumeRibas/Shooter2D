31c5691dd23b1022d67c0efafbc8a9b21e17d64a
Elvis Puertas <epuertasg@gmail.com>
2014-04-23 20:08:44 +0200

    Added method to check multitouch compatibility

--- Shooter2D/src/org/escoladeltreball/shooter2d/MainActivity.java
+++ Shooter2D/src/org/escoladeltreball/shooter2d/MainActivity.java
@@ -6,34 +6,51 @@ import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
+import org.andengine.input.touch.controller.MultiTouch;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
+
+import android.widget.Toast;
 
 public class MainActivity extends SimpleBaseGameActivity
 {
 	private Camera camera;
-    private static final int CAMERA_WIDTH = 720;
-    private static final int CAMERA_HEIGHT = 480;
-	
-    @Override
-    public EngineOptions onCreateEngineOptions()
-    {
-    	camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-        EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, 
-        new FillResolutionPolicy(), camera);
-        return engineOptions;
-    }
+	private static final int CAMERA_WIDTH = 720;
+	private static final int CAMERA_HEIGHT = 480;
 
-    @Override
-    protected void onCreateResources()
-    {
-  
-    }
+	@Override
+	public EngineOptions onCreateEngineOptions()
+	{
+		checkCompatibilityMultiTouch();
+		camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, 
+				new FillResolutionPolicy(), camera);
+		return engineOptions;
+	}
 
-    @Override
-    protected Scene onCreateScene()
-    {
-    	Scene scene = new Scene();
-    	scene.setBackground(new Background(0.09804f, 0.6274f, 0.8784f));
-    	return scene;
-    }
+	@Override
+	protected void onCreateResources()
+	{
+
+	}
+
+	@Override
+	protected Scene onCreateScene()
+	{
+		Scene scene = new Scene();
+		scene.setBackground(new Background(0.09804f, 0.6274f, 0.8784f));
+		return scene;
+	}
+
+	/**
+	 * Comprueba si existe compatibilidad con multitouch
+	 */
+	public void checkCompatibilityMultiTouch(){
+		if(MultiTouch.isSupported(this)) {  
+			if(MultiTouch.isSupportedDistinct(this)) { 
+				Toast.makeText(this, "MultiTouch detected, but your device has problems distinguishing between fingers.", Toast.LENGTH_LONG).show();  
+			}  
+		} else {  
+			Toast.makeText(this, "Sorry your device does NOT support MultiTouch!", Toast.LENGTH_LONG).show();  
+		}  
+	}
 }
