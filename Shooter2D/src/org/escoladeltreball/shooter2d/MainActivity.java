e0685d6555e1e66ecd67fa2161b007dd1506fd77
Elvis Puertas <epuertasg@gmail.com>
2014-04-28 20:11:47 +0200

    Limpieza de código al cargar el mapa TMX

--- Shooter2D/src/org/escoladeltreball/shooter2d/MainActivity.java
+++ Shooter2D/src/org/escoladeltreball/shooter2d/MainActivity.java
@@ -1,22 +1,12 @@
 package org.escoladeltreball.shooter2d;
 
 import java.io.IOException;
-import java.util.ArrayList;
-
-import org.andengine.audio.music.MusicFactory;
-import org.andengine.audio.sound.SoundFactory;
 import org.andengine.engine.camera.BoundCamera;
-import org.andengine.engine.camera.hud.controls.AnalogOnScreenControl;
-import org.andengine.engine.camera.hud.controls.BaseOnScreenControl;
-import org.andengine.engine.camera.hud.controls.AnalogOnScreenControl.IAnalogOnScreenControlListener;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
-import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
-import org.andengine.extension.tmx.TMXLayer;
-import org.andengine.extension.tmx.TMXTiledMap;
 import org.andengine.input.touch.controller.MultiTouch;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
@@ -68,22 +58,11 @@ public class MainActivity extends SimpleBaseGameActivity
 		// Añade los controles con commandos a la escena 
 		scene.setChildScene(UI.getInstance().createAnalogControls(this.camera, this.getVertexBufferObjectManager(), CommandManager.getSetPlayerVelocity(), CommandManager.getDoNothingCommand(), CommandManager.getDoNothingAnalogCommand(), CommandManager.getDoNothingCommand()));
 		// Muestra el mapa en la pantalla
-		TMXTiledMap map = this.mapCreator.loadMap(getAssets(), getTextureManager(), getVertexBufferObjectManager());
-		scene.attachChild(map);
-		this.player = playerLoader.loadPlayer(camera,  getTextureManager(), getAssets(), getVertexBufferObjectManager());
-		// Muentra sobre el mapa rectangulos que son areas de colision
-		ArrayList<Rectangle> rectangles = mapCreator.createUnwalkableObjects(map, vbo);
-		for (Rectangle rect : rectangles){
-			scene.attachChild(rect);
-		}
-		
-		// La camara no execede el tamaño del mapa
-        final TMXLayer tmxLayer = map.getTMXLayers().get(0);
-        this.camera.setBounds(0, 0, tmxLayer.getWidth(), tmxLayer.getHeight());
-        this.camera.setBoundsEnabled(true);
+		scene = this.mapCreator.loadMap(getAssets(), getTextureManager(), getVertexBufferObjectManager(), scene, this.camera);
 		// La camara sigue al jugador
-		this.camera.setChaseEntity(player);
-		scene.attachChild(player);
+		this.player = this.playerLoader.loadPlayer(this.camera,  getTextureManager(), getAssets(), getVertexBufferObjectManager());
+		this.camera.setChaseEntity(this.player);
+		scene.attachChild(this.player);
 		return scene;
 	}
 
--- Shooter2D/src/org/escoladeltreball/shooter2d/MapCreator.java
+++ Shooter2D/src/org/escoladeltreball/shooter2d/MapCreator.java
@@ -2,7 +2,10 @@ package org.escoladeltreball.shooter2d;
 
 import java.util.ArrayList;
 
+import org.andengine.engine.camera.BoundCamera;
 import org.andengine.entity.primitive.Rectangle;
+import org.andengine.entity.scene.Scene;
+import org.andengine.extension.tmx.TMXLayer;
 import org.andengine.extension.tmx.TMXLoader;
 import org.andengine.extension.tmx.TMXObject;
 import org.andengine.extension.tmx.TMXObjectGroup;
@@ -25,7 +28,7 @@ public class MapCreator {
 	 * @param vbo
 	 * @return el mapa cargado para ser añadido a la scene
 	 */
-	public TMXTiledMap loadMap(AssetManager assets, TextureManager texture, VertexBufferObjectManager vbo){
+	public Scene loadMap(AssetManager assets, TextureManager texture, VertexBufferObjectManager vbo, Scene scene, BoundCamera camera){
 		TMXTiledMap mTMXTiledMap = null;
 		try {
 			final TMXLoader tmxLoader = new TMXLoader(assets, texture, TextureOptions.BILINEAR_PREMULTIPLYALPHA, vbo);
@@ -34,23 +37,35 @@ public class MapCreator {
 		} catch (final TMXLoadException e) {
 			Debug.e(e);
 		}
-		return mTMXTiledMap;
+		scene.attachChild(mTMXTiledMap);
+
+		// Muestra sobre el mapa rectangulos que son areas de colision
+		ArrayList<Rectangle> rectangles = createUnwalkableObjects(mTMXTiledMap, vbo, scene);
+		for (Rectangle rect : rectangles){
+			scene.attachChild(rect);
+		}
+
+		// La camara no execede el tamaño del mapa
+		final TMXLayer tmxLayer = mTMXTiledMap.getTMXLayers().get(0);
+		camera.setBounds(0, 0, tmxLayer.getWidth(), tmxLayer.getHeight());
+		camera.setBoundsEnabled(true);
+		return scene;
 	}
 
-/**
- * Crea los rectangulos que no podr�n ser atravesados
- * @param map
- * @param vbo
- * @return
- */
-	public ArrayList<Rectangle> createUnwalkableObjects(TMXTiledMap map, VertexBufferObjectManager vbo){
+	/**
+	 * Crea los rectangulos que no podrán ser atravesados
+	 * @param map
+	 * @param vbo
+	 * @return
+	 */
+	public ArrayList<Rectangle> createUnwalkableObjects(TMXTiledMap map, VertexBufferObjectManager vbo, Scene scene){
 		// Loop through the object groups
 		ArrayList<Rectangle> rectangles = new ArrayList<Rectangle>();
 		for(final TMXObjectGroup group: map.getTMXObjectGroups()) {
 			if(group.getTMXObjectGroupProperties().containsTMXProperty("wall", "true")){
 				// This is our "wall" layer. Create the boxes from it
 				for(final TMXObject object : group.getTMXObjects()) {
-					final Rectangle rect = new Rectangle((float)object.getX(), (float)object.getY(),(float)object.getWidth(), (float)object.getHeight(), vbo);
+					final Rectangle rect = new Rectangle(object.getX(), object.getY(), object.getWidth(), object.getHeight(), vbo);
 					rect.setVisible(true);
 					rect.setColor(Color.RED);
 					rectangles.add(rect);
