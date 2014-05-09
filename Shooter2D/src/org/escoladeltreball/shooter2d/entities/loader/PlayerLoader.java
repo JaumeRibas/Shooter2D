f8f105533d03dd0a8813fd8afd967720e053157e
CSerranoH <arquak@gmail.com>
2014-05-09 17:17:38 +0200

    Mejorada la documentación y comentarios de las clases BulletLoader, PlayerLoader y ZombieLoader.

--- Shooter2D/src/org/escoladeltreball/shooter2d/entities/loader/PlayerLoader.java
+++ Shooter2D/src/org/escoladeltreball/shooter2d/entities/loader/PlayerLoader.java
@@ -1,11 +1,15 @@
 package org.escoladeltreball.shooter2d.entities.loader;
 
+import org.andengine.engine.camera.Camera;
 import org.andengine.entity.scene.Scene;
+import org.andengine.opengl.texture.TextureManager;
 import org.andengine.opengl.texture.region.TiledTextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 import org.escoladeltreball.shooter2d.ResourceManager;
 import org.escoladeltreball.shooter2d.entities.Bullet;
 import org.escoladeltreball.shooter2d.entities.Player;
+
+import android.content.res.AssetManager;
 
 public class PlayerLoader {
 
@@ -14,12 +18,17 @@ public class PlayerLoader {
 	private PlayerLoader(){};
 
 	/**
-	 * La clase PlayerLoader se encarga cargar los recursos del jugador y del propio jugador.
+	 * Devuelve la instancia del player si existe, si no existe, la crea y la devuelve.
+	 * La bala del jugador será la que lleva por defecto en la clase {@link Player}
 	 * 
-	 * @author Carlos Serrano
-	 * @author Elvis Puertas
-	 * @author Jaume Ribas
-	 * @param scene 
+	 * @param camera a {@link Camera}
+	 * @param x a integer, la posición horizontal del player
+	 * @param y a integer, la posición vertical del players
+	 * @param textureManger a {@link TextureManager}
+	 * @param assets a {@link AssetManager}
+	 * @param vertexBufferObjectManager un {@link VertexBufferObjectManager}
+	 * @param scene a {@link Scene}, la escena donde disparará el player
+	 * @return a {@link Player}
 	 */
 	public static Player loadPlayer(float x, float y, VertexBufferObjectManager vertexBufferObjectManager, Scene scene) {
 		if (player == null) {
@@ -33,6 +42,19 @@ public class PlayerLoader {
 
 	}
 	
+	/**
+	 * Devuelve la instancia del player si existe, si no existe, la crea y la devuelve.
+	 * 
+	 * @param camera a {@link Camera}
+	 * @param x a integer, la posición horizontal del player
+	 * @param y a integer, la posición vertical del players
+	 * @param textureManger a {@link TextureManager}
+	 * @param assets a {@link AssetManager}
+	 * @param vertexBufferObjectManager un {@link VertexBufferObjectManager}
+	 * @param scene a {@link Scene}, la escena donde disparará el player
+	 * @param playerbullet a {@link Bullet}, la bala que tendrá el jugador al iniciar la partida.
+	 * @return a {@link Player}
+	 */
 	public static Player loadPlayer(float x, float y, VertexBufferObjectManager vertexBufferObjectManager, Scene scene, Bullet playerbullet) {
 		if (player == null) {
 			TiledTextureRegion pTiledTextureRegion;
@@ -48,7 +70,7 @@ public class PlayerLoader {
 	 * Obtiene la instancia de player.
 	 * Puede ser null si todavia no se ha creado.
 	 * 
-	 * @return
+	 * @return a {@link Player}
 	 */
 	public static Player getPlayer() {
 		return player;
