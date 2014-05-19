da99d105169e1a30951bb4a83cdb3a04b0e8482a
CSerranoH <arquak@gmail.com>
2014-05-19 19:35:44 +0200

    Ahora el zombie tiene un body mas grande, lo que hace que sea mas fácil acertarles.

--- Shooter2D/src/org/escoladeltreball/shooter2d/constants/HPConstants.java
+++ Shooter2D/src/org/escoladeltreball/shooter2d/constants/HPConstants.java
@@ -5,11 +5,11 @@ public class HPConstants {
 	public static int HUMAN_HEALTH = 10;
 	public static float HUMAN_WALK_SPEED = 4f;
 	
-	public static int ZOMBIE_HEALTH = 7;
+	public static int ZOMBIE_HEALTH = 6;
 	public static int ZOMBIE_STRENGH = 2;
 	public static int ZOMBIE_SIGHT_RADIUS = 200;
 	public static float ZOMBIE_WALK_SPEED = 2.5f;
-	public static final float ZOMBIE_ATTACK_TIMER = 3f;
+	public static final float ZOMBIE_ATTACK_TIMER = 0.75f;
 
 	
 }
