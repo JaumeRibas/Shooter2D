51c05715a667703984c10b2b85f12dd77309aed6
CSerranoH <arquak@gmail.com>
2014-05-21 18:45:40 +0200

    Las constantes de HPConstants y Spriteconstants ahora son finales.

--- Shooter2D/src/org/escoladeltreball/shooter2d/constants/HPConstants.java
+++ Shooter2D/src/org/escoladeltreball/shooter2d/constants/HPConstants.java
@@ -2,13 +2,13 @@ package org.escoladeltreball.shooter2d.constants;
 
 public class HPConstants {
 	
-	public static int HUMAN_HEALTH = 10;
-	public static float HUMAN_WALK_SPEED = 4f;
+	public static final int HUMAN_HEALTH = 10;
+	public static final float HUMAN_WALK_SPEED = 4f;
 	
-	public static int ZOMBIE_HEALTH = 6;
-	public static int ZOMBIE_STRENGH = 2;
-	public static int ZOMBIE_SIGHT_RADIUS = 200;
-	public static float ZOMBIE_WALK_SPEED = 2.5f;
+	public static final int ZOMBIE_HEALTH = 6;
+	public static final int ZOMBIE_STRENGH = 2;
+	public static final int ZOMBIE_SIGHT_RADIUS = 200;
+	public static final float ZOMBIE_WALK_SPEED = 2.5f;
 	public static final float ZOMBIE_ATTACK_TIMER = 0.75f;
 
 	
--- Shooter2D/src/org/escoladeltreball/shooter2d/constants/SpriteConstants.java
+++ Shooter2D/src/org/escoladeltreball/shooter2d/constants/SpriteConstants.java
@@ -12,20 +12,20 @@ public final class SpriteConstants {
 	
 	public static final String SPRITE_FOLDER = "gfx/";
 	
-	public static String PLAYER_SPRITE = SPRITE_FOLDER + "player.png";
-	public static int PLAYER_SPRITE_COLUMNS = 2;
-	public static int PLAYER_SPRITE_ROWS = 4;
+	public static final String PLAYER_SPRITE = SPRITE_FOLDER + "player.png";
+	public static final int PLAYER_SPRITE_COLUMNS = 2;
+	public static final int PLAYER_SPRITE_ROWS = 4;
 
-	public static String ZOMBIE_SPRITE = SPRITE_FOLDER + "character.png";
-	public static int ZOMBIE_SPRITE_COLUMNS = 6;
-	public static int ZOMBIE_SPRITE_ROWS = 1;
+	public static final String ZOMBIE_SPRITE = SPRITE_FOLDER + "character.png";
+	public static final int ZOMBIE_SPRITE_COLUMNS = 6;
+	public static final int ZOMBIE_SPRITE_ROWS = 1;
 	
 	public static final String CONTROL_KNOB_SPRITE = SPRITE_FOLDER + "onscreen_control_knob.png";
 	public static final String CONTROL_BASE_SPRITE = SPRITE_FOLDER + "onscreen_control_base.png";
 
 	public static final String SPLASH_IMAGE = SPRITE_FOLDER + "splash.png";
-	public static String BULLET_SPRITE = SPRITE_FOLDER + "bullet.png";
-	public static int BULLET_SPRITE_COLUMNS = 2;
-	public static int BULLET_SPRITE_ROWS = 1;
+	public static final String BULLET_SPRITE = SPRITE_FOLDER + "bullet.png";
+	public static final int BULLET_SPRITE_COLUMNS = 2;
+	public static final int BULLET_SPRITE_ROWS = 1;
 
 }
